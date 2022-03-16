b97dd9423f5f305742750484813d369caf401fd4
ganeshmajhi <ganeshmajhi@gmail.com>
2022-03-16 23:54:53 +0530

    added offer page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Explore from './pages/Explore'
 import Category from './pages/Category'
 import Navbar from './components/Navbar'
 import ForgetPassword from './pages/ForgotPassword'
-import Offer from './pages/Offer'
+import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import SignIN from './pages/SignIN'
 import SignUP from './pages/SignUP'
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore/>}/>
         <Route path='/forget-password' element={<ForgetPassword/>}/>
-        <Route path='/offer' element={<Offer/>}/>
+        <Route path='/offers' element={<Offers/>}/>
         <Route path='/category/:categoryName' element ={<Category/>}/>
         <Route path='/profile' element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
